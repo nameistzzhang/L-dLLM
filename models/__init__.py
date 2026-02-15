191043b5b1595876799cb97bc6cce1c795fce04e
T.Z.Zhang <ericzhang@stu.pku.edu.cn>
2026-02-15 17:30:38 +0800

    exp: try load model from pretrained

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .llada import LLaDAModelLM, LLaDAConfig
 from .mmada import MMadaConfig, MMadaModelLM
 from .sdar import SDARModel, SDARForCausalLM, SDARConfig
 from .dream import DreamTokenizer, DreamModel, DreamConfig
+from .latent_llada import LatentLLaDAModelLM, LLaDAConfig
